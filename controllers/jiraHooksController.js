5240df831f70226e534b1cb397a5c17f9d5f0ac1
Stanislav Blokh <Dnepr_horeca@list.ru>
2018-05-25 16:54:23 +0300

    add workLog

--- controllers/jiraHooksController.js
+++ controllers/jiraHooksController.js
@@ -1,13 +1,27 @@
 var request = require('request');
 
 exports.hookParse = function  (hook) {
+    let message;
     console.log(hook, 'controller');
-    const message = {
-        channel: "StasMMintegration",
+if (hook.webhookEvent == 'worklog_created') {
+    message = {
+        // channel: getChannel(hook),
+        channel: 'StasMMintegration',
         username: "JiraBot",
-        icon_url : "https://www.mattermost.org/wp-content/uploads/2016/04/icon.png",
-        text: hook.user.displayName + " created a task" + hook.issue.key + '/n ' + hook.issue.fields.description,
-    }
+        icon_url: "https://www.mattermost.org/wp-content/uploads/2016/04/icon.png",
+        text: createWorklogMessage(hook)
+
+    };
+} else {
+    message = {
+        // channel: getChannel(hook),
+        channel: 'StasMMintegration',
+        username: "JiraBot",
+        icon_url: "https://www.mattermost.org/wp-content/uploads/2016/04/icon.png",
+        text: createTaskMessage(hook)
+
+    };
+}
     console.log(message, 'message');
     var clientServerOptions = {
         uri: 'https://mattermost.lanars.com/hooks/phg368euej81fb3mop4ub1ho5r',
@@ -21,7 +35,46 @@ exports.hookParse = function  (hook) {
         console.log(error,response.body);
         return;
     });
-
 };
 
-// module.exports = [hookParse];
+function createTaskMessage (hook) {
+    let action;
+    let  description;
+    try {
+        description =  hook.issue.fields.description;
+    } catch (e) {
+        description = ''
+    }
+    const task = hook.issue.key;
+    const name = hook.user.displayName;
+    switch (hook.issue_event_type_name)  {
+        case 'issue_assigned':
+            action = 'assigned a task';
+            break;
+        case 'issue_created':
+            action = 'created a task';
+            break;
+        case 'comment_created':
+            action ='commented a task';
+            break;
+        default:
+            action = 'does some magic with'
+    }
+    let parsedMessage = name +' ' + action + ' ' + task + '\n' + description;
+    return parsedMessage;
+};
+
+function createWorklogMessage(hook) {
+    const creator = hook.worklog.author.displayName;
+    const action = 'created a worklog to task ';
+    const task = hook.worklog.issueId;
+    worklogMessage = creator + ' ' + action + task;
+    return worklogMessage;
+}
+
+//
+// function getChannel(hook) {
+//
+//     return channel
+// }
+
--- routes/jiraHooks.js
+++ routes/jiraHooks.js
@@ -3,10 +3,7 @@ var router = express.Router();
 var parser = require('../controllers/jiraHooksController');
 
 router.post('/', function(req, res, next) {
-    console.log(req.query);
-    // console.log(req.body);
     parser.hookParse(req.body);
-    // res.send(req.query.user_id);
 });
 
 module.exports = router;
